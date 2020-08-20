b20e0ded98223b6138fd3560d0de3c2768c5a279
Bruno <brunormferreira@hotmail.com>
2020-08-20 15:50:28 -0300

    add(feat): tests to header and layout component

--- components/header/__tests__/Header.test.tsx
+++ components/header/__tests__/Header.test.tsx
@@ -1,1 +1,13 @@
 import React from 'react'
+import { render } from '@testing-library/react'
+
+import Header from '../Header.component'
+
+describe('Header component', () => {
+  it('should render component', () => {
+    const { getByText } = render(<Header />)
+    const linkElement = getByText(/home/i)
+
+    expect(linkElement).toBeInTheDocument()
+  })
+})
