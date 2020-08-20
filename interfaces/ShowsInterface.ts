0c74bfa379b50ba9c87220792f60191851133f64
Bruno <brunormferreira@hotmail.com>
2020-08-20 14:59:48 -0300

    add(feat): show interface

---
+++ interfaces/ShowsInterface.ts
@@ -0,0 +1,11 @@
+export default interface ShowsInterface {
+  id?: number
+  url: string
+  name: string
+  type?: string
+  language?: string
+  genres?: any[]
+  status?: string
+  summary: string
+  image: any
+}
