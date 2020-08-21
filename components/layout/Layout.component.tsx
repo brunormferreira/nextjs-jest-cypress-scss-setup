07314dcb02f15741c2bca2947eff8dcca94b02b1
Bruno <brunormferreira@hotmail.com>
2020-08-21 11:42:11 -0300

    add(feat): star work on toggle theme

--- components/layout/Layout.component.tsx
+++ components/layout/Layout.component.tsx
@@ -1,26 +1,43 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Head from 'next/head'
 import Header from '../header/Header.component'
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD',
-}
+import lightTheme from '../themes-layout/light.module.scss'
+import darkTheme from '../themes-layout/dark.module.scss'
+
+import styles from './Layout.module.scss'
 
 type Props = {
   children: React.ReactNode
 }
 
 const Layout: FC<Props> = ({ children }: Props) => {
+  const [darkMode, setDarkMode] = useState(false)
+  const isDark = !!darkMode
+
+  const cn = (arr: string[]) => arr.filter(Boolean).join(' ')
+
   return (
-    <div style={layoutStyle}>
+    <div
+      className={cn([
+        styles.background,
+        isDark && styles['dark-bg'],
+        isDark ? darkTheme.theme : lightTheme.theme,
+      ])}
+    >
       <Head>
-        <title>brunormferreira nextjs - Setup</title>
+        <title>Nextjs Application</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <div>{children}</div>
+      <button
+        className={cn([styles.button, isDark && styles['dark-button']])}
+        type="button"
+        onClick={() => setDarkMode(!darkMode)}
+      >
+        Switch theme
+      </button>
     </div>
   )
 }
