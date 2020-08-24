98e54786b73e1e8dc1f492bc6b83ad0f34fe03cd
Bruno <brunormferreira@hotmail.com>
2020-08-24 13:29:52 -0300

    add(feat): button component and test

--- components/button/Button.component.tsx
+++ components/button/Button.component.tsx
@@ -3,8 +3,8 @@ import { FC } from 'react'
 type Props = {
   type: string | any
   onClick: (event: React.ChangeEvent) => void
-  className: string
-  children: React.ReactChildren
+  className?: string
+  children: string | React.ReactChildren
 }
 
 const Button: FC<Props> = ({ className, type, children }: Props) => (
--- components/button/__tests__/Button.test.tsx
+++ components/button/__tests__/Button.test.tsx
@@ -3,10 +3,16 @@ import { render } from '@testing-library/react'
 
 import Button from '../Button.component'
 
+const MOCK = {
+  onClick: (_event: React.ChangeEvent<HTMLInputElement>) => {},
+  type: 'button',
+  children: 'dummy-text',
+}
+
 describe('Button component', () => {
   it('should render component', () => {
-    const { getByText } = render(<Button />)
-    const linkElement = getByText(/TODO/i)
+    const { getByText } = render(<Button {...MOCK} />)
+    const linkElement = getByText(/dummy/i)
 
     expect(linkElement).toBeInTheDocument()
   })
